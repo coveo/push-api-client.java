191acb1853501ccf479176ba68393d77b3bf856b
Olivier Lamothe <olamothe@coveo.com>
2021-06-28 11:05:39 -0400

    feat: create platform client class (#2)
    https://coveord.atlassian.net/browse/CDX-385

--- src/main/java/com/coveo/testlocally/TestingLocally.java
+++ src/main/java/com/coveo/testlocally/TestingLocally.java
@@ -1,12 +1,29 @@
 package com.coveo.testlocally;
 
 import com.coveo.pushapiclient.DocumentBuilder;
+import com.coveo.pushapiclient.Source;
+import com.coveo.pushapiclient.SourceVisibility;
+import io.github.cdimascio.dotenv.Dotenv;
 
+import java.io.IOException;
+import java.net.http.HttpResponse;
 import java.util.Date;
 
 public class TestingLocally {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
         DocumentBuilder doc = new DocumentBuilder("https://perdu.com", "the title").withData("this is searchable").withDate(new Date());
-        System.out.println(doc.marshal());
+        Source source = new Source(dotenv.get("API_KEY"), dotenv.get("ORG_ID"));
+        try {
+            HttpResponse res = source.create("testlocaljava", SourceVisibility.SECURED);
+            System.out.println(String.format("Status: %s %s", res.statusCode(), res.body().toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
