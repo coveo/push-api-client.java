95ebf811e42a2a93e1e5d645cbc9333fac12616e
Olivier Lamothe <olamothe@coveo.com>
2021-06-30 12:47:41 -0400

    feat: final all the things

--- src/main/java/com/coveo/pushapiclient/SecurityIdentityModel.java
+++ src/main/java/com/coveo/pushapiclient/SecurityIdentityModel.java
@@ -1,7 +1,7 @@
 package com.coveo.pushapiclient;
 
 public class SecurityIdentityModel extends SecurityIdentityModelBase {
-    public IdentityModel[] members;
+    public final IdentityModel[] members;
 
     public SecurityIdentityModel(IdentityModel[] members, IdentityModel identity, IdentityModel[] wellKnowns) {
         super(identity, wellKnowns);
