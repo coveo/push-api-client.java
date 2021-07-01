06d46cf1db137d0a3d47d255b555aed1eb72f106
Olivier Lamothe <olamothe@coveo.com>
2021-07-01 10:00:44 -0400

    use record

--- src/main/java/com/coveo/pushapiclient/BatchUpdate.java
+++ src/main/java/com/coveo/pushapiclient/BatchUpdate.java
@@ -4,15 +4,7 @@ import com.google.gson.JsonObject;
 
 import java.util.List;
 
-public class BatchUpdate {
-    private List<DocumentBuilder> addOrUpdate;
-    private List<DocumentBuilder> delete;
-
-    public BatchUpdate(List<DocumentBuilder> addOrUpdate, List<DocumentBuilder> delete) {
-        this.addOrUpdate = addOrUpdate;
-        this.delete = delete;
-    }
-
+public record BatchUpdate(List<DocumentBuilder> addOrUpdate, List<DocumentBuilder> delete) {
     public BatchUpdateRecord marshal() {
         return new BatchUpdateRecord(
                 this.addOrUpdate.stream().map(documentBuilder -> documentBuilder.marshalJsonObject()).toArray(JsonObject[]::new),
