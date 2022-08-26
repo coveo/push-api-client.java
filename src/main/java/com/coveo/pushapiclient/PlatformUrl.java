4e32ef154b976ddd32e518f08a3e61d4b24f4b05
ylakhdar <ylakhdar@coveo.com>
2022-08-26 12:36:06 -0400

    update doc and sample

--- src/main/java/com/coveo/pushapiclient/PlatformUrl.java
+++ src/main/java/com/coveo/pushapiclient/PlatformUrl.java
@@ -5,6 +5,12 @@ public class PlatformUrl {
     private final Environment environment;
     private final Region region;
 
+    /**
+     * @param environment The environment platform of your organization
+     * @param region      The physical center of your organization
+     *
+     * @see https://docs.coveo.com/en/2976
+     */
     public PlatformUrl(Environment environment, Region region) {
         this.environment = environment;
         this.region = region;
--- src/main/java/com/coveo/pushapiclient/Source.java
+++ src/main/java/com/coveo/pushapiclient/Source.java
@@ -9,7 +9,9 @@ public class Source {
     PlatformClient platformClient;
 
     /**
-     * @param apiKey         An apiKey capable of pushing documents and managing sources in a Coveo organization. See [Manage API Keys](https://docs.coveo.com/en/1718).
+     * @param apiKey         An apiKey capable of pushing documents and managing
+     *                       sources in a Coveo organization. See [Manage API
+     *                       Keys](https://docs.coveo.com/en/1718).
      * @param organizationId The Coveo Organization identifier.
      */
     public Source(String apiKey, String organizationId) {
@@ -17,9 +19,11 @@ public class Source {
     }
 
     /**
-     * @param apiKey         An apiKey capable of pushing documents and managing sources in a Coveo organization. See [Manage API Keys](https://docs.coveo.com/en/1718).
+     * @param apiKey         An apiKey capable of pushing documents and managing
+     *                       sources in a Coveo organization. See [Manage API
+     *                       Keys](https://docs.coveo.com/en/1718).
      * @param organizationId The Coveo Organization identifier.
-     * @param platformUrl    The platform Url
+     * @param platformUrl
      */
     public Source(String apiKey, String organizationId, PlatformUrl platformUrl) {
         this.platformClient = new PlatformClient(apiKey, organizationId, platformUrl);
@@ -42,17 +46,22 @@ public class Source {
      * Create a new push source.
      *
      * @param name             The name of the source to create
-     * @param sourceVisibility The security option that should be applied to the content of the source. See [Content Security](https://docs.coveo.com/en/1779).
+     * @param sourceVisibility The security option that should be applied to the
+     *                         content of the source. See [Content
+     *                         Security](https://docs.coveo.com/en/1779).
      * @return
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> create(String name, SourceVisibility sourceVisibility) throws IOException, InterruptedException {
+    public HttpResponse<String> create(String name, SourceVisibility sourceVisibility)
+            throws IOException, InterruptedException {
         return this.platformClient.createSource(name, sourceVisibility);
     }
 
     /**
-     * Create or update a security identity. See [Adding a Single Security Identity](https://docs.coveo.com/en/167) and [Security Identity Models](https://docs.coveo.com/en/139).
+     * Create or update a security identity. See [Adding a Single Security
+     * Identity](https://docs.coveo.com/en/167) and [Security Identity
+     * Models](https://docs.coveo.com/en/139).
      *
      * @param securityProviderId
      * @param securityIdentityModel
@@ -60,12 +69,15 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> createOrUpdateSecurityIdentity(String securityProviderId, SecurityIdentityModel securityIdentityModel) throws IOException, InterruptedException {
+    public HttpResponse<String> createOrUpdateSecurityIdentity(String securityProviderId,
+            SecurityIdentityModel securityIdentityModel) throws IOException, InterruptedException {
         return this.platformClient.createOrUpdateSecurityIdentity(securityProviderId, securityIdentityModel);
     }
 
     /**
-     * Create or update a security identity alias. See [Adding a Single Alias](https://docs.coveo.com/en/142) and [User Alias Definition Examples](https://docs.coveo.com/en/46).
+     * Create or update a security identity alias. See [Adding a Single
+     * Alias](https://docs.coveo.com/en/142) and [User Alias Definition
+     * Examples](https://docs.coveo.com/en/46).
      *
      * @param securityProviderId
      * @param securityIdentityAliasModel
@@ -73,12 +85,14 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> createOrUpdateSecurityIdentityAlias(String securityProviderId, SecurityIdentityAliasModel securityIdentityAliasModel) throws IOException, InterruptedException {
+    public HttpResponse<String> createOrUpdateSecurityIdentityAlias(String securityProviderId,
+            SecurityIdentityAliasModel securityIdentityAliasModel) throws IOException, InterruptedException {
         return this.platformClient.createOrUpdateSecurityIdentityAlias(securityProviderId, securityIdentityAliasModel);
     }
 
     /**
-     * Delete a security identity. See [Disabling a Single Security Identity](https://docs.coveo.com/en/84).
+     * Delete a security identity. See [Disabling a Single Security
+     * Identity](https://docs.coveo.com/en/84).
      *
      * @param securityProviderId
      * @param securityIdentityDelete
@@ -86,12 +100,14 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> deleteSecurityIdentity(String securityProviderId, SecurityIdentityDelete securityIdentityDelete) throws IOException, InterruptedException {
+    public HttpResponse<String> deleteSecurityIdentity(String securityProviderId,
+            SecurityIdentityDelete securityIdentityDelete) throws IOException, InterruptedException {
         return this.platformClient.deleteSecurityIdentity(securityProviderId, securityIdentityDelete);
     }
 
     /**
-     * Update the status of a Push source. See [Updating the Status of a Push Source](https://docs.coveo.com/en/35).
+     * Update the status of a Push source. See [Updating the Status of a Push
+     * Source](https://docs.coveo.com/en/35).
      *
      * @param sourceId
      * @param status
@@ -99,12 +115,14 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> updateSourceStatus(String sourceId, PushAPIStatus status) throws IOException, InterruptedException {
+    public HttpResponse<String> updateSourceStatus(String sourceId, PushAPIStatus status)
+            throws IOException, InterruptedException {
         return this.platformClient.updateSourceStatus(sourceId, status);
     }
 
     /**
-     * Delete old security identities. See [Disabling Old Security Identities](https://docs.coveo.com/en/33).
+     * Delete old security identities. See [Disabling Old Security
+     * Identities](https://docs.coveo.com/en/33).
      *
      * @param securityProviderId
      * @param batchDelete
@@ -112,12 +130,14 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> deleteOldSecurityIdentities(String securityProviderId, SecurityIdentityDeleteOptions batchDelete) throws IOException, InterruptedException {
+    public HttpResponse<String> deleteOldSecurityIdentities(String securityProviderId,
+            SecurityIdentityDeleteOptions batchDelete) throws IOException, InterruptedException {
         return this.platformClient.deleteOldSecurityIdentities(securityProviderId, batchDelete);
     }
 
     /**
-     * Manage batches of security identities. See [Manage Batches of Security Identities](https://docs.coveo.com/en/55).
+     * Manage batches of security identities. See [Manage Batches of Security
+     * Identities](https://docs.coveo.com/en/55).
      *
      * @param securityProviderId
      * @param batchConfig
@@ -125,12 +145,14 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> manageSecurityIdentities(String securityProviderId, SecurityIdentityBatchConfig batchConfig) throws IOException, InterruptedException {
+    public HttpResponse<String> manageSecurityIdentities(String securityProviderId,
+            SecurityIdentityBatchConfig batchConfig) throws IOException, InterruptedException {
         return this.platformClient.manageSecurityIdentities(securityProviderId, batchConfig);
     }
 
     /**
-     * Adds or updates an individual item in a push source. See [Adding a Single Item in a Push Source](https://docs.coveo.com/en/133).
+     * Adds or updates an individual item in a push source. See [Adding a Single
+     * Item in a Push Source](https://docs.coveo.com/en/133).
      *
      * @param sourceId
      * @param docBuilder
@@ -138,13 +160,19 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> addOrUpdateDocument(String sourceId, DocumentBuilder docBuilder) throws IOException, InterruptedException {
-        CompressionType compressionType = docBuilder.getDocument().compressedBinaryData != null ? docBuilder.getDocument().compressedBinaryData.getCompressionType() : CompressionType.UNCOMPRESSED;
-        return this.platformClient.pushDocument(sourceId, docBuilder.marshal(), docBuilder.getDocument().uri, compressionType);
+    public HttpResponse<String> addOrUpdateDocument(String sourceId, DocumentBuilder docBuilder)
+            throws IOException, InterruptedException {
+        CompressionType compressionType = docBuilder.getDocument().compressedBinaryData != null
+                ? docBuilder.getDocument().compressedBinaryData.getCompressionType()
+                : CompressionType.UNCOMPRESSED;
+        return this.platformClient.pushDocument(sourceId, docBuilder.marshal(), docBuilder.getDocument().uri,
+                compressionType);
     }
 
     /**
-     * Deletes a specific item from a Push source. Optionally, the child items of that item can also be deleted. See [Deleting an Item in a Push Source](https://docs.coveo.com/en/171).
+     * Deletes a specific item from a Push source. Optionally, the child items of
+     * that item can also be deleted. See [Deleting an Item in a Push
+     * Source](https://docs.coveo.com/en/171).
      *
      * @param sourceId
      * @param documentId
@@ -153,12 +181,14 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> deleteDocument(String sourceId, String documentId, Boolean deleteChildren) throws IOException, InterruptedException {
+    public HttpResponse<String> deleteDocument(String sourceId, String documentId, Boolean deleteChildren)
+            throws IOException, InterruptedException {
         return this.platformClient.deleteDocument(sourceId, documentId, deleteChildren);
     }
 
     /**
-     * Manage batches of items in a push source. See [Manage Batches of Items in a Push Source](https://docs.coveo.com/en/90)
+     * Manage batches of items in a push source. See [Manage Batches of Items in a
+     * Push Source](https://docs.coveo.com/en/90)
      *
      * @param sourceId
      * @param batchUpdate
@@ -166,7 +196,8 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> batchUpdateDocuments(String sourceId, BatchUpdate batchUpdate) throws IOException, InterruptedException {
+    public HttpResponse<String> batchUpdateDocuments(String sourceId, BatchUpdate batchUpdate)
+            throws IOException, InterruptedException {
         HttpResponse<String> resFileContainer = this.platformClient.createFileContainer();
         FileContainer fileContainer = new Gson().fromJson(resFileContainer.body(), FileContainer.class);
         this.platformClient.uploadContentToFileContainer(fileContainer, new Gson().toJson(batchUpdate.marshal()));
@@ -174,7 +205,9 @@ public class Source {
     }
 
     /**
-     * Manages pushing batches of Security Identities to a File Container, then into Coveo. See [Manage Batches of Security Identities](https://docs.coveo.com/en/55)
+     * Manages pushing batches of Security Identities to a File Container, then into
+     * Coveo. See [Manage Batches of Security
+     * Identities](https://docs.coveo.com/en/55)
      *
      * @param securityProviderId
      * @param batchIdentity
@@ -182,21 +215,26 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public SecurityIdentityBatchResponse batchUpdateSecurityIdentities(String securityProviderId, BatchIdentity batchIdentity) throws IOException, InterruptedException {
+    public SecurityIdentityBatchResponse batchUpdateSecurityIdentities(String securityProviderId,
+            BatchIdentity batchIdentity) throws IOException, InterruptedException {
         SecurityIdentityBatchResponse securityIdentityBatchResponse = new SecurityIdentityBatchResponse();
         HttpResponse<String> resFileContainer = this.platformClient.createFileContainer();
         FileContainer fileContainer = new Gson().fromJson(resFileContainer.body(), FileContainer.class);
         String batchIdJson = new Gson().toJson(batchIdentity.marshal());
-        securityIdentityBatchResponse.s3Response = this.platformClient.uploadContentToFileContainer(fileContainer, batchIdJson);
-        if (securityIdentityBatchResponse.s3Response.statusCode() >= 200 && securityIdentityBatchResponse.s3Response.statusCode() <= 299) { //maybe just 200 or 202
+        securityIdentityBatchResponse.s3Response = this.platformClient.uploadContentToFileContainer(fileContainer,
+                batchIdJson);
+        if (securityIdentityBatchResponse.s3Response.statusCode() >= 200
+                && securityIdentityBatchResponse.s3Response.statusCode() <= 299) { // maybe just 200 or 202
             SecurityIdentityBatchConfig batchConfig = new SecurityIdentityBatchConfig(fileContainer.fileId, 0l);
-            securityIdentityBatchResponse.batchResponse = this.manageSecurityIdentities(securityProviderId, batchConfig);
+            securityIdentityBatchResponse.batchResponse = this.manageSecurityIdentities(securityProviderId,
+                    batchConfig);
         }
         return securityIdentityBatchResponse;
     }
 
     /**
-     * Creates a File Container. [Creating a File Container](https://docs.coveo.com/en/43)
+     * Creates a File Container. [Creating a File
+     * Container](https://docs.coveo.com/en/43)
      *
      * @return
      * @throws IOException
@@ -208,7 +246,8 @@ public class Source {
     }
 
     /**
-     * Push file to a File Container. [Using the compressedBinaryDataFileId Property](https://docs.coveo.com/en/69)
+     * Push file to a File Container. [Using the compressedBinaryDataFileId
+     * Property](https://docs.coveo.com/en/69)
      *
      * @param fileContainer
      * @param fileAsBytes
@@ -216,7 +255,8 @@ public class Source {
      * @throws IOException
      * @throws InterruptedException
      */
-    public HttpResponse<String> pushBinaryToFileContainer(FileContainer fileContainer, byte[] fileAsBytes) throws IOException, InterruptedException {
+    public HttpResponse<String> pushBinaryToFileContainer(FileContainer fileContainer, byte[] fileAsBytes)
+            throws IOException, InterruptedException {
         return this.platformClient.pushBinaryToFileContainer(fileContainer, fileAsBytes);
     }
 }
