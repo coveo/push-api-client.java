7d5dc2c56992f66dc364f6dec66c0ec498ce59f6
ylakhdar <ylakhdar@coveo.com>
2023-05-30 14:30:30 -0400

    draft PushService

---
+++ src/main/java/com/coveo/pushapiclient/PushServiceInternal.java
@@ -0,0 +1,24 @@
+package com.coveo.pushapiclient;
+
+import java.io.IOException;
+
+public class PushServiceInternal {
+    private DocumentUploadQueue queue;
+
+    public PushServiceInternal(DocumentUploadQueue queue) {
+        this.queue = queue;
+    }
+
+    public void addOrUpdate(DocumentBuilder document) throws IOException, InterruptedException {
+        this.queue.add(document);
+    }
+
+    public void delete(DeleteDocument document) throws IOException, InterruptedException {
+        this.queue.add(document);
+    }
+
+    public void close() throws IOException, InterruptedException {
+        queue.flush();
+    }
+
+}
