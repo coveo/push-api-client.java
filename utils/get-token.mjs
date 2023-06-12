b27eb59dff1ea1ee771221337b6a36c178a46917
ylakhdar <ylakhdar@coveo.com>
2023-06-12 10:59:26 -0400

    get installation token

---
+++ utils/get-token.mjs
@@ -0,0 +1,18 @@
+import {createAppAuth} from '@octokit/auth-app';
+import {setSecret} from '@actions/core'
+
+const auth = createAppAuth({
+  appId: process.env.RELEASER_APP_ID,
+  privateKey: process.env.RELEASER_PRIVATE_KEY,
+  clientId: process.env.RELEASER_CLIENT_ID,
+  clientSecret: process.env.RELEASER_CLIENT_SECRET,
+});
+
+// Retrieve installation access token
+const {token} = await auth({
+  type: 'installation',
+  installationId: process.env.RELEASER_INSTALLATION_ID,
+});
+
+setSecret(token);
+exportVariable('RELEASE_TOKEN', token);
